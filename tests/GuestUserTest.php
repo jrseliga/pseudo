116eb029780c8fd92fb417912939916c45e6b630
Justin Seliga <jseliga@agilesdesign.com>
2017-06-28 21:21:54 +0000

    Setup testing. Test GuestContract is returned by Auth

---
+++ tests/GuestUserTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Pseudo\Contracts\GuestContract;
+use Orchestra\Testbench\TestCase;
+
+class GuestUserTest extends TestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('auth.guards.web.driver', 'pseudo');
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [\Pseudo\Providers\PseudoServiceProvider::class];
+    }
+
+    /**
+     * Test that Laravel Auth returns instance of GuestContract
+     */
+    public function test_auth_returns_guest()
+    {
+        $this->assertInstanceOf(GuestContract::class, Auth::user());
+    }
+}
